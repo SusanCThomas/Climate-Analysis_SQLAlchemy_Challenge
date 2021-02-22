45ac7861e1a46c3075f4f5d81e4b9407aacb32f3
Susan C Thomas <susancthomas@Susans-MBP.lan>
2021-02-21 23:55:13 -0500

    database setup, create app and code to run the flask application

--- climate.py
+++ climate.py
@@ -8,3 +8,23 @@ from sqlalchemy.orm import Session
 from sqlalchemy import create_engine, func
 
 from flask import Flask, jsonify
+
+
+# Database Setup
+
+engine = create_engine("sqlite:///Resources/hawaii.sqlite")
+
+Base = automap_base()
+Base.prepare(engine, reflect=True)
+
+Measurement = Base.classes.measurement
+Station = Base.classes.station
+
+
+# Create App
+app = Flask(__name__)
+
+
+# Run the Flask Application
+if__name__ == '__main__':
+    app.run(debug=True)
