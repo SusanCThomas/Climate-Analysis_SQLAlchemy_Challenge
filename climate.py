0b24eaa5c69131bb2c25b16a49e2825f02e5a953
Susan C Thomas <susancthomas@Susans-MBP.lan>
2021-02-22 00:42:18 -0500

    added precipitation and stations routes

--- climate.py
+++ climate.py
@@ -25,6 +25,55 @@ Station = Base.classes.station
 app = Flask(__name__)
 
 
+# Define Endpoints and List Available Routes
+
+@app.route("/")
+def home():
+    return """
+    <h1>Welcome to the Hawaii Climate Analysis API!</h1><br/>
+    <h2>Available Routes:</h2><br/>
+        /api/v1.0/precipitation<br/>
+        /api/v1.0/station<br/>
+        /api/v1.0/tobs<br/>
+        /api/v1.0/start=YYYY-MM-DD<br/>
+        api/v1.0/start=YYYY-MM-DD/end=YYYY-MM-DD;
+    """
+
+
+# Precipitation Route
+
+@app.route("/api/v1.0/precipitation")
+def precipitation():
+    session = Session(engine)
+
+    target_date = dt.date(2017, 8, 23)
+    delta = dt.timedelta(days=365)
+    query_data = target_date - delta
+
+    results = session.query(Measurement.date, Measurement.prcp).\
+        filter(Measurement.date > query_data).order_by(Measurement.date.desc()).all()
+    session.close()
+
+    precipitation_list = []
+    for i in range(len(results)):
+        precipitation_dict = {}
+        precipitation_dict[results[i][0]] = results[i][1]
+        precipitation_list.append(precipitation_dict)
+
+    return jsonify(precipitation_list)
+
+
+# Stations Route
+@app.route("/api/v1.0/station")
+def station():
+    session = Session(engine)
+    results = session.query(Station.name).all()
+    session.close()
+
+    station_ = list(np.ravel(results))
+
+    return jsonify(station_)
+
 # Run the Flask Application
-if__name__ == '__main__':
+if __name__ == '__main__':
     app.run(debug=True)
